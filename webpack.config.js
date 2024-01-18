a0e17fbfdb9201b85cce8ee4a1a8f1e26893b9b7
pmhxxhsj <minho7952@naver.com>
2024-01-19 02:07:20 +0900

    feat: ts-loader 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,24 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     filename: "clearly-log.js",
     path: path.resolve(__dirname, "dist"),
     library: "LoggingSDK",
     libraryTarget: "umd",
     globalObject: "this",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
 };
