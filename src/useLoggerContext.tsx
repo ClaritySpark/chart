49ec80401f6e82bca2137252ef1e74298c87ad2a
pmhxxhsj <minho7952@naver.com>
2024-01-20 22:38:05 +0900

    feat: 화살표 함수 => 일반 함수로 변경

--- src/useLoggerContext.tsx
+++ src/useLoggerContext.tsx
@@ -4,14 +4,16 @@ import { LogProviderProps, UseLoggerReturn } from './types/logger';
 
 const ClearlyLoggerContext = React.createContext<UseLoggerReturn | null>(null);
 
-export const useLoggerContext = (): UseLoggerReturn =>
-  React.useContext(ClearlyLoggerContext) as UseLoggerReturn;
+export function useLoggerContext(): UseLoggerReturn {
+  return React.useContext(ClearlyLoggerContext) as UseLoggerReturn;
+}
 
-export const LogProvider = (props: LogProviderProps) => {
+export function LogProvider(props: LogProviderProps): JSX.Element {
   const { children, ...data } = props;
+
   return (
     <ClearlyLoggerContext.Provider value={data as unknown as UseLoggerReturn}>
       {children}
     </ClearlyLoggerContext.Provider>
   );
-};
+}
