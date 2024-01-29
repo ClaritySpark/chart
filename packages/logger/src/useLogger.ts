5494ccd03dcad8a738a6d7d039f83820283e1a52
pmhxxhsj <minho7952@naver.com>
2024-01-30 01:29:40 +0900

    feat: apply useLogger consoleLogger package

--- packages/logger/src/useLogger.ts
+++ packages/logger/src/useLogger.ts
@@ -1,15 +1,27 @@
-import { UseLoggerReturn } from "./types";
+import { ConsoleLogger } from "@clarity/utils";
+import type { UseLoggerReturn } from "./types";
+import type { LogLevel } from "@clarity/types";
+
+const DEFAULT_LOG_LEVELS: LogLevel[] = [
+  "verbose",
+  "debug",
+  "log",
+  "warn",
+  "error",
+  "fatal",
+];
 
 export function useLogger(): UseLoggerReturn {
+  // TODO: Change to encapsulated logic
+  const logger = new ConsoleLogger(DEFAULT_LOG_LEVELS);
+
   return {
     // TODO: { logId, params } save database
     mount: ({ logId, params }) => {
-      console.log("logId", logId);
-      console.log("params", params);
+      logger.log("mount log", logId, params);
     },
     click: ({ logId, params }) => {
-      console.log("logId", logId);
-      console.log("params", params);
+      logger.log("click log", logId, params);
     },
   };
 }
