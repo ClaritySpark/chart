68f9b598dd6d06aba12b598451ede73d90f762d7
pmhxxhsj <minho7952@naver.com>
2024-01-30 01:37:27 +0900

    feat: change Logger class

--- packages/logger/src/useLogger.ts
+++ packages/logger/src/useLogger.ts
@@ -1,27 +1,18 @@
-import { ConsoleLogger } from "@clarity/utils";
+import { Logger } from "@clarity/utils";
 import type { UseLoggerReturn } from "./types";
-import type { LogLevel } from "@clarity/types";
-
-const DEFAULT_LOG_LEVELS: LogLevel[] = [
-  "verbose",
-  "debug",
-  "log",
-  "warn",
-  "error",
-  "fatal",
-];
 
 export function useLogger(): UseLoggerReturn {
-  // TODO: Change to encapsulated logic
-  const logger = new ConsoleLogger(DEFAULT_LOG_LEVELS);
-
   return {
-    // TODO: { logId, params } save database
+    /**
+     * TODO
+     * Change to encapsulated logic
+     * { logId, params } save database
+     */
     mount: ({ logId, params }) => {
-      logger.log("mount log", logId, params);
+      Logger.log("mount log", logId, params);
     },
     click: ({ logId, params }) => {
-      logger.log("click log", logId, params);
+      Logger.log("click log", logId, params);
     },
   };
 }
